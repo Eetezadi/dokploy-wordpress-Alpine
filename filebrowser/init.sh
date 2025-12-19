0afe2c1272ff23c5a521d26c2f7810d1c7862f6a
Al-Mamun Talukder <admin@itsmereal.com>
2025-12-19 19:51:26 +0800

    Fix FileBrowser Dockerfile - use Alpine base with filebrowser binary

--- filebrowser/init.sh
+++ filebrowser/init.sh
@@ -1,14 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 set -e
+
+FB_BIN="/usr/local/bin/filebrowser"
 
 # If database doesn't exist, create it with default user
 if [ ! -f /database/filebrowser.db ]; then
     echo "Initializing FileBrowser database..."
-    /filebrowser config init --database /database/filebrowser.db
-    /filebrowser config set --database /database/filebrowser.db --root /srv
-    /filebrowser users add admin admin --database /database/filebrowser.db --perm.admin
+    $FB_BIN config init --database /database/filebrowser.db
+    $FB_BIN config set --database /database/filebrowser.db --root /srv --address 0.0.0.0 --port 80
+    $FB_BIN users add admin admin --database /database/filebrowser.db --perm.admin
     echo "FileBrowser initialized with user: admin / password: admin"
 fi
 
 # Start FileBrowser
-exec /filebrowser --database /database/filebrowser.db
+exec $FB_BIN --database /database/filebrowser.db
